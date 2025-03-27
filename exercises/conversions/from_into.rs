30f730dde50a35405ee5a71666bf3aea0f13c7a0
101117 <2368073614@qq.com>
2025-03-27 22:25:52 +0800

    update

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
+
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let mut ss=s.split(',');
+        let sss=ss.next().unwrap();
+        let name=match sss.len(){
+            0=> { return Person::default(); },
+            _=>sss.to_string(),
+        };
+        let a=match ss.next() {
+            Some(s)=>{s},
+            None=>{return Person::default();},
+        };
+        match ss.next() {
+            Some(_)=>{return Person::default();},
+            None=>{},
+        };
+        match a.parse::<usize>(){
+            Ok(age)=>Person { name: name.to_string(), age },
+            Err(_)=>{return Person::default();},
+        }
     }
 }
 
